090fed3b331e29881c8666a90ae6e3596d8fdff4
Masaki Hara <ackie.h.gmai@gmail.com>
2022-06-08 22:25:55 +0900

    LSP: report parse errors

--- lsp/src/lib.rs
+++ lsp/src/lib.rs
@@ -1,5 +1,7 @@
 use async_trait::async_trait;
 use parking_lot::RwLock;
+use rbbardiche::parse;
+use tokio::task::spawn_blocking;
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer};
@@ -52,33 +54,33 @@ impl LanguageServer for Backend {
     }
 
     async fn did_open(&self, params: DidOpenTextDocumentParams) {
-        if self.has_publish_diagnostics() {
-            self.client
-                .publish_diagnostics(
-                    params.text_document.uri.clone(),
-                    vec![Diagnostic {
-                        range: Range {
-                            start: Position {
-                                line: 0,
-                                character: 0,
-                            },
-                            end: Position {
-                                line: 0,
-                                character: 1,
-                            },
-                        },
-                        severity: None,
-                        code: None,
-                        code_description: None,
-                        source: None,
-                        message: "Something something something".to_string(),
-                        related_information: None,
-                        tags: None,
-                        data: None,
-                    }],
-                    Some(params.text_document.version),
+        if !self.has_publish_diagnostics() {
+            return;
+        }
+        parse_and_report(
+            self.client.clone(),
+            params.text_document.text.clone(),
+            VersionedTextDocumentIdentifier {
+                uri: params.text_document.uri.clone(),
+                version: params.text_document.version,
+            },
+        )
+        .await;
+    }
+
+    async fn did_change(&self, params: DidChangeTextDocumentParams) {
+        if !self.has_publish_diagnostics() {
+            return;
+        }
+        for change in &params.content_changes {
+            if change.range.is_none() {
+                parse_and_report(
+                    self.client.clone(),
+                    change.text.clone(),
+                    params.text_document.clone(),
                 )
                 .await;
+            }
         }
     }
 
@@ -95,4 +97,91 @@ impl LanguageServer for Backend {
     async fn shutdown(&self) -> Result<()> {
         Ok(())
     }
+}
+
+async fn parse_and_report(
+    client: Client,
+    text: String,
+    text_document: VersionedTextDocumentIdentifier,
+) {
+    let task = spawn_blocking(move || parse(text.as_bytes()));
+    let (_, errors) = match task.await {
+        Ok(x) => x,
+        Err(e) => {
+            let e = if e.is_panic() {
+                let e = e.into_panic();
+                if let Some(&e) = e.downcast_ref::<&'static str>() {
+                    e.to_owned()
+                } else if let Some(e) = e.downcast_ref::<String>() {
+                    e.clone()
+                } else {
+                    "Box<Any>".to_string()
+                }
+            } else {
+                e.to_string()
+            };
+
+            client
+                .publish_diagnostics(
+                    text_document.uri.clone(),
+                    vec![Diagnostic {
+                        range: Range {
+                            start: Position {
+                                line: 0,
+                                character: 0,
+                            },
+                            end: Position {
+                                line: 0,
+                                character: 1,
+                            },
+                        },
+                        severity: None,
+                        code: None,
+                        code_description: None,
+                        source: None,
+                        message: e,
+                        related_information: None,
+                        tags: None,
+                        data: None,
+                    }],
+                    Some(text_document.version),
+                )
+                .await;
+            return;
+        }
+    };
+    let diagnostics = errors
+        .iter()
+        .map(|error| Diagnostic {
+            range: Range {
+                start: Position {
+                    line: 0,
+                    character: error.range().0 as u32,
+                },
+                end: Position {
+                    line: 0,
+                    character: error.range().1 as u32,
+                },
+            },
+            severity: Some(if error.is_error() {
+                DiagnosticSeverity::ERROR
+            } else {
+                DiagnosticSeverity::WARNING
+            }),
+            code: None,
+            code_description: None,
+            source: None,
+            message: error.to_string(),
+            related_information: None,
+            tags: None,
+            data: None,
+        })
+        .collect::<Vec<_>>();
+    client
+        .publish_diagnostics(
+            text_document.uri.clone(),
+            diagnostics,
+            Some(text_document.version),
+        )
+        .await;
 }
